924b1c100e45ebf139d068ba2a9eb36df73b1d8f
Ricardo <misakasweb@gmail.com>
2019-11-06 22:55:03 +0800

    logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -45,4 +45,16 @@ class LoginController extends Controller
             'message'=>'登录成功'
         ];
     }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return [
+            'code'=>200,
+            'message'=>'登录成功'
+        ];
+    }
 }
